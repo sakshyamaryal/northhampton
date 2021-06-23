953c256c2af5413b7aee775095e5eea2b7b5fe0d
sakshyamaryal <sakshyamaryal1@gmail.com>
2021-06-23 08:42:54 +0545

    java script required for hamburger icon

---
+++ script.js
@@ -0,0 +1,8 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	//document.getElementsByClass("siter-bg").classList.toggle("change-bg")
+	
+	//document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+/*https://www.youtube.com/watch?v=F-mvXKKoSCU*/
